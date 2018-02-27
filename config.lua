bc1dd0504cb1508331346e8fd7accd37a84be900
ElPumpo <samo_kol@hotmail.com>
2018-02-27 19:05:13 +0100

    Changed prices of robbing bank

--- config.lua
+++ config.lua
@@ -1,23 +1,23 @@
 Config = {}
 Config.Locale = 'en'
-Config.NumberOfCopsRequired = 4
+Config.NumberOfCopsRequired = 6
 
 Banks = {
 	["fleeca"] = {
 		position = { ['x'] = 147.04908752441, ['y'] = -1044.9448242188, ['z'] = 29.36802482605 },
-		reward = math.random(15000,30000),
+		reward = math.random(100000,200000),
 		nameofbank = "Fleeca Bank",
 		lastrobbed = 0
 	},
 	["fleeca2"] = {
 		position = { ['x'] = -2957.6674804688, ['y'] = 481.45776367188, ['z'] = 15.697026252747 },
-		reward = math.random(5000,15000),
+		reward = math.random(40000,100000),
 		nameofbank = "Fleeca Bank (Highway)",
 		lastrobbed = 0
 	},
 	["blainecounty"] = {
 		position = { ['x'] = -107.06505584717, ['y'] = 6474.8012695313, ['z'] = 31.62670135498 },
-		reward = math.random(5000,15000),
+		reward = math.random(40000,100000),
 		nameofbank = "Blaine County Savings",
 		lastrobbed = 0
 	},
