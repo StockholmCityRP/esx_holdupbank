13d278862d02a81d1edd2cbb971f65ad22d11958
Kasper <35449118+KasperE1@users.noreply.github.com>
2018-03-06 11:52:43 +0100

    Create sv.lua

---
+++ locales/sv.lua
@@ -0,0 +1,23 @@
+Locales['sv'] = {
+
+	['robbery_cancelled'] = 'Rånet kommer att avbrytas, du kommer inte att få något!',
+	['robbery_successful'] = 'Rånet klart, du fick ~g~$',
+	['bank_robbery'] = 'Bankrån',
+	['press_to_rob'] = 'Tryck ~INPUT_CONTEXT~ för att råna ~b~',
+	['robbery_of'] = 'Bankrån: ~r~',
+	['seconds_remaining'] = '~w~ sekunder återstår',
+	['robbery_cancelled_at'] = '~r~ Rån avbrytet på: ~b~',
+	['robbery_has_cancelled'] = '~r~ Rånet har avbrytits: ~b~',
+	['already_robbed'] = 'Det var nydligen ett rån här, vänta ',
+	['seconds'] = 'sekunder.',
+	['rob_in_prog'] = '~r~ Rån pågår på: ~b~',
+	['started_to_rob'] = 'Du startade rånet ',
+	['do_not_move'] = ', rör dig inte!',
+	['alarm_triggered'] = 'Larmet har utlösts',
+	['hold_pos'] = 'håll banken i 5 minuter och pengarna är dina!',
+	['robbery_complete'] = '~r~ Rånet klart.~s~ ~h~ Spring! ',
+	['robbery_complete_at'] = '~r~ Rån klar på: ~b~',
+	['min_two_police'] = 'Antal poliser som behövs: ',
+	['robbery_already'] = '~r~ det är redan rån.',
+
+}
